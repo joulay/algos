56f220f916a727505cf7ae9594a36c1b4e9e15b8
Julie K <julieskim160@gmail.com>
2018-08-07 17:44:56 -0700

    classic fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,15 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
-    for ( let i = 0; i < n.length; i++) {
-        if(n % 3 === 0) {
-            return "fizz"
-        }
-        if (n % 5 === 0) {
-            return "buzzz"
-        }
+function fizzBuzz(n) {
+    for (let i = 1; i <=n; i++) {
+      if(i %3==0 && i%5===0) {
+        console.log('fizzbuzz')
+      } else if (i%3 === 0) {
+        console.log('fizz')
+      } else if (i%5===0) {
+        console.log('buzz')
+      } else {
+        console.log(i)
+      }
     }
+  
+      
+  };
+  
 
 
 
