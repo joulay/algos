23d014cdeb2b9e261537811d1ed8d56ea7839ef7
Julie K <julieskim160@gmail.com>
2018-09-17 15:54:34 -0700

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let ch = {};
+    let max = 0;
+    let maxStr = '';
+    for (let x of str) { //iterate through array or string
+        if(ch[x]) {
+            ch[x]++;
+        } else {
+            ch[x]=1;
+        }
+    }
+    for (let x in ch) { //iterate through object
+        if (ch[x] > max ) {
+            max = ch[x]
+            maxStr = x;
+        }
+    }
+    return maxStr;
+}
 
+maxChar("123111")
 module.exports = maxChar;
