2507223c18b7ca24ca6ee10c935ac6fe89370871
Julie K <julieskim160@gmail.com>
2018-09-18 18:44:07 -0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    } 
+
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        } 
+    }
+    return true;
+}
+
+//helper function character map for both strings
+function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) { //character that is not number or lower case, replace with empty string
+        charMap[char] = charMap[char] + 1 || 1;
+    } 
+
+    return charMap;
+}
+
+
+
+console.log(anagrams('rail safety', 'fairy tales'))
 
 module.exports = anagrams;
