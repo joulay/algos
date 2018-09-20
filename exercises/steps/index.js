70209e0d741f49422d2f53a5fc17379ad971d90c
Julie K <julieskim160@gmail.com>
2018-09-19 19:17:26 -0700

    other solution for steps algo

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -32,3 +32,21 @@ function steps(n) {
 }
 
 module.exports = steps;
+
+
+function steps(n, row = 0, stair = '') {
+    if(n === row) {
+        return;
+    }
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    if(stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+}
+
