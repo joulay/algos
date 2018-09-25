0b3350cbece9ded037d93fc3b588adfc6f3c178e
Julie K <julieskim160@gmail.com>
2018-09-24 17:36:43 -0700

    start on matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,12 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+
+    for (let i = 0; i < n; i ++) {
+        results.push([]);
+    }
+}
 
 module.exports = matrix;
